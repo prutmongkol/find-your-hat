59958106027f2b0cce208f58c0f5fa6e21f6909c
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2023-12-22 10:14:27 +0700

    complete basic requirement

--- main.js
+++ main.js
@@ -7,79 +7,170 @@ const pathCharacter = '*';
 const playerCharacter = '☺';
 
 const controlScheme = ['w', 'a', 's', 'd']; // up, left, down, right
-let gameState = true;
 
 class Field {
-  constructor(field) {
-    this._field = field; // 2D Array
-    this._playerXPosition = 0;
-    this._playerYPosition = 0;
-  }
-
-  print() {
-    let fieldString = '';
-    this._field.forEach( y => {
-      fieldString += y.join('') + '\n';
-    });
-    console.log(fieldString);
-  }
-
-  isInBoundaries() {
-    const xBoundary = this._field[0].length;
-    const yBoundary = this._field.length;
-    
-    return this._playerXPosition > -1 && this._playerXPosition < xBoundary && this._playerYPosition > -1 && this._playerYPosition < yBoundary;
-  }
-
-  isInHole() {
-    const x = this._playerXPosition;
-    const y = this._playerYPosition;
-    const currentTile = this._field[y][x];
-    console.log(currentTile);
-  }
-
-  movePlayer(validDirection) {
-    let direction;
-    do {
-      direction = prompt('Which way? ').toLowerCase();
-    } while( !validDirection.includes(direction) );
-  
-    switch (direction) {
-      case validDirection[0]:
-        this._playerYPosition--;
-        break;
-      case validDirection[1]:
-        this._playerXPosition--;
-        break;
-      case validDirection[2]:
-        this._playerYPosition++;
-        break;
-      case validDirection[3]:
-        this._playerXPosition++;
-        break;
+    constructor(field) {
+        this._field = field; // 2D Array
+        this._playerXPosition = 0;
+        this._playerYPosition = 0;
     }
-  }
 
+    print() {
+        let fieldString = '';
+        this._field.forEach( y => {
+        fieldString += y.join('') + '\n';
+        });
+        console.log(fieldString);
+    }
 
+    isInBoundaries() {
+        const x = this._playerXPosition;
+        const y = this._playerYPosition;
+        const xBoundary = this._field[0].length;
+        const yBoundary = this._field.length;
+        
+        return (x > -1) && (x < xBoundary) && (y > -1) && (y < yBoundary);
+    }
+
+    isOnHole() {
+        const x = this._playerXPosition;
+        const y = this._playerYPosition;
+        const currentTile = this._field[y][x];
+        
+        return currentTile === hole;
+    }
+
+    isOnHat() {
+        const x = this._playerXPosition;
+        const y = this._playerYPosition;
+        const currentTile = this._field[y][x];
+        
+        return currentTile === hat;
+    }
+
+    movePlayer(validDirection) {
+        let direction;
+        do {
+        direction = prompt('Which way? ').toLowerCase();
+        } while( !validDirection.includes(direction) );
+    
+        const x = this._playerXPosition;
+        const y = this._playerYPosition
+        this._field[y][x] = pathCharacter;
+
+        switch (direction) {
+        case validDirection[0]:
+            this._playerYPosition--;
+            break;
+        case validDirection[1]:
+            this._playerXPosition--;
+            break;
+        case validDirection[2]:
+            this._playerYPosition++;
+            break;
+        case validDirection[3]:
+            this._playerXPosition++;
+            break;
+        }
+    }
+
+    revertPosition() {
+        const x = this._playerXPosition;
+        const y = this._playerYPosition;
+        const xBoundary = this._field[0].length;
+        const yBoundary = this._field.length;
+
+        if (x === -1) {
+            this._playerXPosition++;
+        } else if (x === xBoundary) {
+            this._playerXPosition--;
+        } else if (y === -1) {
+            this._playerYPosition++;
+        } else if (y === yBoundary) {
+            this._playerYPosition--;
+        }
+    }
+
+    gameLoop() {
+        let gameState = true;
+        let isWon;
+
+        do {
+            this.print();
+            this.movePlayer(controlScheme);
+
+            if (this.isOnHole()) {
+                gameState = false;
+                isWon = false;
+            } else if (this.isOnHat()) {
+                gameState = false;
+                isWon = true;
+            } else if (!this.isInBoundaries()) {
+                console.log("You can't move out-of-bound!");
+                this.revertPosition();
+                const x = this._playerXPosition;
+                const y = this._playerYPosition
+                this._field[y][x] = playerCharacter;
+            } else {
+                const x = this._playerXPosition;
+                const y = this._playerYPosition
+                this._field[y][x] = playerCharacter;
+            }
+        } while(gameState);
+
+        if (isWon) {
+            console.log("You've found your hat!");
+        } else {
+            console.log("Oh no! You've fallen into a hole!");
+        }
+    }
+
+    static generateField(x = 10, y = 10, holePercent = 0.2) {
+        const fieldArray = [];
+        for (let i = 0; i < y; i++) {
+            const xAxis = [];
+            for (let i = x; i > 0; i--) {
+                xAxis.push(fieldCharacter);
+            }
+            fieldArray.push(xAxis);
+        }
+        
+        for (let holeAmount = Math.floor(x * y * holePercent); holeAmount > 0; holeAmount--) {
+            let randomX;
+            let randomY;
+            do {
+                randomX = Math.floor(Math.random() * x);
+                randomY = Math.floor(Math.random() * y);
+            } while (fieldArray[randomY][randomX] === hole || (randomX === 0 && randomY === 0));
+            fieldArray[randomY][randomX] = hole;
+        }
+        
+        let randomX;
+        let randomY;
+        do {
+            randomX = Math.floor(Math.random() * x);
+            randomY = Math.floor(Math.random() * y);
+        } while (fieldArray[randomY][randomX] === hole || (randomX === 0 || randomY === 0));
+        fieldArray[randomX][randomY] = hat;
+
+        fieldArray[0][0] = playerCharacter;
+
+        return new Field(fieldArray);
+    }
 }
 
 const startGame = () => {
-  const myField = new Field([
-    [playerCharacter, fieldCharacter, hole],
-    [fieldCharacter, hole, fieldCharacter],
-    [fieldCharacter, hat, fieldCharacter]
-  ]);
-  console.log(
-    `- Input -\n` +
-    `    ↑    \n` +
-    `    W    \n` +
-    `← A D S →\n` +
-    `    ↓    \n` +
-    `---------`
-  );
-  myField.print(); 
-  myField.movePlayer(controlScheme);
+    const myField = Field.generateField();
+    console.log(
+        `- Input -\n` +
+        `    ↑    \n` +
+        `    W    \n` +
+        `← A D S →\n` +
+        `    ↓    \n` +
+        `---------`
+    );
 
+    myField.gameLoop();
 }
 
 startGame();
